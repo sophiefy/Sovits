7e74ebb447ce8ac3f873d0430c1e24a1c6715c2d
Francis-Hu <87693204+Francis-Komizu@users.noreply.github.com>
2022-08-31 17:46:52 +0800

    support multi-speaker training

--- data_utils.py
+++ data_utils.py
@@ -114,6 +114,120 @@ class UnitAudioCollate():
             return unit_padded, unit_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths, ids_sorted_decreasing
         return unit_padded, unit_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths
 
+"""Multi speaker version"""
+class UnitAudioSpeakerLoader(torch.utils.data.Dataset):
+    """
+        1) loads audio, speaker_id, speech unit pairs
+        2) computes spectrograms from audio files.
+    """
+    def __init__(self, unit_sid_audio_pairs, hparams):
+        self.unit_sid_audio_pairs = load_unit_audio_pairs(unit_sid_audio_pairs)
+        self.max_wav_value = hparams.max_wav_value
+        self.sampling_rate = hparams.sampling_rate
+        self.filter_length  = hparams.filter_length
+        self.hop_length     = hparams.hop_length
+        self.win_length     = hparams.win_length
+        self.sampling_rate  = hparams.sampling_rate
+
+        random.seed(1234)
+        random.shuffle(self.unit_sid_audio_pairs)
+        self._filter()
+
+    def _filter(self):
+        lengths = []
+        for audio_path, _, _ in self.unit_sid_audio_pairs:
+            lengths.append(os.path.getsize(audio_path) // (2 * self.hop_length))
+        self.lengths = lengths
+
+    def get_unit_sid_audio_pair(self, unit_sid_audio_pair):
+        # separate filename, speaker_id and text
+        audio_path, sid, unit_path = unit_sid_audio_pair[0], unit_sid_audio_pair[1], unit_sid_audio_pair[2]
+        unit = np.load(unit_path)
+        unit = torch.LongTensor(unit)
+        spec, wav = self.get_audio(audio_path)
+        sid = self.get_sid(sid)
+        return (unit, spec, wav, sid)
+
+    def get_audio(self, filename):
+        audio, sampling_rate = load_wav_to_torch(filename)
+        if sampling_rate != self.sampling_rate:
+            raise ValueError("{} {} SR doesn't match target {} SR".format(
+                sampling_rate, self.sampling_rate))
+        audio_norm = audio / self.max_wav_value
+        audio_norm = audio_norm.unsqueeze(0)
+        spec_filename = filename.replace(".wav", ".spec.pt")
+        if os.path.exists(spec_filename):
+            spec = torch.load(spec_filename)
+        else:
+            spec = spectrogram_torch(audio_norm, self.filter_length,
+                self.sampling_rate, self.hop_length, self.win_length,
+                center=False)
+            spec = torch.squeeze(spec, 0)
+            torch.save(spec, spec_filename)
+        return spec, audio_norm
+
+    def get_sid(self, sid):
+        sid = torch.LongTensor([int(sid)])
+        return sid
+
+    def __getitem__(self, index):
+        return self.get_unit_sid_audio_pair(self.unit_sid_audio_pairs[index])
+
+    def __len__(self):
+        return len(self.unit_sid_audio_pairs)
+
+class UnitAudioSpeakerCollate():
+    """ Zero-pads model inputs and targets
+    """
+    def __init__(self, return_ids=False):
+        self.return_ids = return_ids
+
+    def __call__(self, batch):
+        """Collate's training batch from normalized text, audio and speaker identities
+        PARAMS
+        ------
+        batch: [unit, spec_normalized, wav_normalized, sid]
+        """
+        # Right zero-pad all one-hot text sequences to max input length
+        _, ids_sorted_decreasing = torch.sort(
+            torch.LongTensor([x[1].size(1) for x in batch]),
+            dim=0, descending=True)
+
+        max_unit_len = max([len(x[0]) for x in batch])
+        max_spec_len = max([x[1].size(1) for x in batch])
+        max_wav_len = max([x[2].size(1) for x in batch])
+
+        unit_lengths = torch.LongTensor(len(batch))
+        spec_lengths = torch.LongTensor(len(batch))
+        wav_lengths = torch.LongTensor(len(batch))
+        sid = torch.LongTensor(len(batch))
+
+        unit_padded = torch.LongTensor(len(batch), max_unit_len, 256)
+        spec_padded = torch.FloatTensor(len(batch), batch[0][1].size(0), max_spec_len)
+        wav_padded = torch.FloatTensor(len(batch), 1, max_wav_len)
+        unit_padded.zero_()
+        spec_padded.zero_()
+        wav_padded.zero_()
+        for i in range(len(ids_sorted_decreasing)):
+            row = batch[ids_sorted_decreasing[i]]
+
+            unit = row[0]
+            unit_padded[i, :unit.size(0)] = unit
+            unit_lengths[i] = unit.size(0)
+
+            spec = row[1]
+            spec_padded[i, :, :spec.size(1)] = spec
+            spec_lengths[i] = spec.size(1)
+
+            wav = row[2]
+            wav_padded[i, :, :wav.size(1)] = wav
+            wav_lengths[i] = wav.size(1)
+
+            sid[i] = row[3]
+
+        if self.return_ids:
+            return unit_padded, unit_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths, sid, ids_sorted_decreasing
+        return unit_padded, unit_lengths, spec_padded, spec_lengths, wav_padded, wav_lengths, sid
 
 class DistributedBucketSampler(torch.utils.data.distributed.DistributedSampler):
     """
