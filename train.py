85de73aa6cd42a70613625d2dca7b6e0dabb6046
Francis-Hu <87693204+Francis-Komizu@users.noreply.github.com>
2022-09-07 17:45:10 +0800

    finetune

--- train.py
+++ train.py
@@ -232,11 +232,11 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
 
             if global_step % hps.train.eval_interval == 0:
                 evaluate(hps, net_g, eval_loader, writer_eval)
-                model_name = os.path.split(hps.model_dir)[1]
+                # model_name = os.path.split(hps.model_dir)[1]
                 utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch,
-                                      os.path.join(hps.model_dir, "G_{}.pth".format(model_name)))
+                                      os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
                 utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch,
-                                      os.path.join(hps.model_dir, "D_{}.pth".format(model_name)))
+                                      os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
         global_step += 1
 
     if rank == 0:
